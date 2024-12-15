d09d7e36232f422a487b89ca183581e581b9e9b6
galg-mend <gal.gernitzky@mend.io>
2024-12-15 14:12:18 +0200

    test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,8 +7,8 @@ repositories {
 maven {
 url = uri("https://pkgs.dev.azure.com/galgernitzky/_packaging/galgernitzky/maven/v1")
 credentials {
-username = System.getenv("AZURE_USERNAME") ?: settings.extra["datev.artifactory.username"]?.toString()
-password = System.getenv("AZURE_PASSWORD") ?: settings.extra["datev.artifactory.password"]?.toString()
+username = System.getenv("AZURE_USERNAME") ?: "${"AZURE_USERNAME"}" ?: settings.extra["datev.artifactory.username"]?.toString()
+password = System.getenv("AZURE_PASSWORD") ?: "${"AZURE_PASSWORD"}" ?: settings.extra["datev.artifactory.password"]?.toString()
 }
 }
 }
